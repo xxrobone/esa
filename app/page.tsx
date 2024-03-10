5728726a2a3f47487f7169f460727ce4687a5670
robonexx <robertwagar@gmail.com>
2024-03-10 18:47:26 +0100

    grid lines animated, icons, effects

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,26 @@ import React, { useContext, useState } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import { CursorContext } from '@/components/Cursor/CursorProvider';
+import GridLines from '@/components/Gridlines';
 
 export default function Home() {
   const { setSize } = useContext(CursorContext);
   return (
     <main className={styles.main}>
+      <GridLines />
       <div className={styles.description}>
         <p
           onMouseEnter={() => {
-            setSize('regular');
-            console.log('regular')
+            setSize('large');
+            console.log('regular');
           }}
           onMouseLeave={() => setSize('small')}
         >
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          Pioneering Web3 Casino Gaming
         </p>
         <div
           onMouseEnter={() => {
-            setSize('large');
-            console.log('large')
+            setSize('bordered');
           }}
           onMouseLeave={() => setSize('small')}
         >
@@ -31,78 +31,90 @@ export default function Home() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By <h2>DeGaming</h2>
           </a>
         </div>
       </div>
 
-      <div className={styles.center}
-       onMouseEnter={() => {
-        setSize('xlarge');
-        console.log('xlarge')
-      }}
-      onMouseLeave={() => setSize('small')}
+      <div
+        className={styles.center}
+        onMouseEnter={() => {
+          setSize('xlarge');
+          console.log('xlarge');
+        }}
+        onMouseLeave={() => setSize('small')}
       >
         <Image
           className={styles.logo}
-          src='/next.svg'
-          alt='Next.js Logo'
+          src='/LogoWhite.png'
+          alt='Degaming Logo'
           width={180}
           height={37}
           priority
         />
+        <p>
+          {' '}
+          DeGaming is positioned at the vanguard of blockchain-integrated
+          gambling solutions. We provide an interactive, safe, and transparent
+          space for our gaming clients to experience the future of iGaming
+        </p>
       </div>
 
       <div className={styles.grid}>
         <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href='https://degaming.io'
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
-
           onMouseEnter={() => {
             setSize('mask');
-            console.log('mask')
           }}
           onMouseLeave={() => setSize('small')}
-
         >
           <h2>
-            Docs <span>-&gt;</span>
+            DeGaming <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>
+            {' '}
+            We`re an online platform. We`re pioneering a new ecnomic model for
+            the industry.
+          </p>
         </a>
 
         <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href='#'
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
+          onMouseEnter={() => {
+            setSize('degaming');
+          }}
+          onMouseLeave={() => setSize('small')}
         >
           <h2>
-            Learn <span>-&gt;</span>
+            About <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>Learn about our mission!</p>
         </a>
 
         <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          href='https://gls-development.vercel.app/'
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
+          onMouseEnter={() => {
+            setSize('crypto');
+            console.log('mask3');
+          }}
+          onMouseLeave={() => setSize('small')}
         >
           <h2>
-            Templates <span>-&gt;</span>
+            GLS<span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p>
+            Explore Web3 Casino Bankrolls And Seize The Opportunity To Grow Your
+            Investments In An Exciting, Evolving Market.{' '}
+          </p>
         </a>
 
         <a
@@ -110,15 +122,22 @@ export default function Home() {
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
+          onMouseEnter={() => {
+            setSize('crypto2');
+            console.log('mask4');
+          }}
+          onMouseLeave={() => setSize('small')}
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            Deploy a Casino <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Instantly deploy your next Casino and start growing your own market!
+            with our customizable SDK
           </p>
         </a>
       </div>
+      <div className='bg'></div>
     </main>
   );
 }
